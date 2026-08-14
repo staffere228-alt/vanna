d412abdc4f5e1df560cd28409fa23f8f1f44dbd2
staffere228-alt <staffere228@gmail.com>
2026-08-15 02:43:43 +0500

    Update farm.lua

--- farm.lua
+++ farm.lua
@@ -410,7 +410,7 @@ local BoxController = Shop:WaitForChild("BoxController")
 
 local boxes = {
   "KnifeBox1", "KnifeBox2", "KnifeBox3", "KnifeBox4", "KnifeBox5", "GunBox1", "GunBox3"
-}
+"SummerBox26"}
 
 local currencies = {"Coins", "Gems", "Key"}
 
